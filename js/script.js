a0b7190cdfaf91a8dbcfd8a0ae089159812ac88a
MicheleRai <rainonemichele1999@gmail.com>
2022-10-27 15:28:11 +0200

    esercizio base

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 // Descrizione:
 // Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
 // Quindi rivediamo l’esercizio visto insieme, ma questa volta con il ciclo while al posto del ciclo for. Facciamo attenzione a non dimenticare tutte le differenze implementative che il ciclo while ci impone, in particolare alla variabile di indice.
+
+ const arrListaSpesa = ["pane","pasta","ceci","latte"];
+
+ const eleListaSpesa = document.querySelector('.lista-spesa');
+
+let i = 0
+while (i < arrListaSpesa.length) {
+	eleListaSpesa.innerHTML += `<li>${arrListaSpesa[i]}</li>`;
+	i++;
+}
